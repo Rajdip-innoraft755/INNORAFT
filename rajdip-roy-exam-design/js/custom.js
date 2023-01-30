a3618ed60dcae2ca216069c7fc7202640dcc0b55
Rajdip-innoraft755 <rajdip.roy@innoraft.com>
2023-01-30 17:41:23 +0530

    Initial commit

---
+++ rajdip-roy-exam-design/js/custom.js
@@ -0,0 +1,24 @@
+$('.box-1').hover(function(){
+    $('.heading1').css({'color':'#febb0c',});
+},function() {
+    $('.heading1').css({'color':'#0b8087',});
+});
+
+$('.box-2').hover(function(){
+    $('.heading2').css({'color':'#febb0c',});
+},function() {
+    $('.heading2').css({'color':'#0b8087',});
+});
+
+$('.box-3').hover(function(){
+    $('.heading3').css({'color':'#febb0c',});
+},function() {
+    $('.heading3').css({'color':'#0b8087',});
+});
+
+
+$('.box-4').hover(function(){
+    $('.heading4').css({'color':'#febb0c',});
+},function() {
+    $('.heading4').css({'color':'#0b8087',});
+});
